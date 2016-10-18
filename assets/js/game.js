ee13a114daf11192b68c4ae3f4d7446e3a4aee7d
Saleema Ali <sali1390@gmail.com>
2016-10-17 21:25:11 -0400

    Added While Loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,34 +1,47 @@
+var playAgain = true;
+console.log("hello");
+
 var wins = 0;
 var losses = 0;
-var randomLetter =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var randomLetter = [];
 var userPicks = [];
 
-var compPick = randomLetter[Math.floor(Math.random() * randomLetter.length)];
+// while (playAgain == true) {
+	console.log("hello");
 
-for (i = 0; i < 10;) {
-	var guessesLeft = 9;
-	document.onkeydown = function(event) {
-		var userInput = String.fromCharCode(event.keyCode).toLowerCase();
-		if (userInput == compPick) {
-			userPicks.push(userInput);
-			wins++;
-			alert("You Win!");
-			// break;
-		}
-		else if (userInput !== compPick) {
-			guessesLeft--;
-		}
-		console.log(compPick);
-		console.log(userInput);
-		console.log(wins);
-		console.log(losses);
-		console.log(guessesLeft);
-		console.log("----------");
-	}
+	randomLetter =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+	userPicks = [];
 
-	if (guessesLeft == 0) {
-		losses++;
-		alert("You Lose");
+	var compPick = randomLetter[Math.floor(Math.random() * randomLetter.length)];
+
+	for (i = 0; i < 10;) {
+		var guessesLeft = 9;
+		document.onkeydown = function(event) {
+			var userInput = String.fromCharCode(event.keyCode).toLowerCase();
+			if (userInput == compPick) {
+				userPicks.push(userInput);
+				wins++;
+				playAgain = confirm("You Win! Do you want to play again?");
+				// break;
+			}
+			else if (userInput !== compPick) {
+				userPicks.push(userInput);
+				guessesLeft--;
+			}
+			console.log(compPick);
+			console.log(userInput);
+			console.log(wins);
+			console.log(losses);
+			console.log(guessesLeft);
+			console.log(userPicks);
+			console.log("----------");
+		}
+
+		if (guessesLeft == 0) {
+			losses++;
+			alert("You Lose");
+			playAgain = confirm("Do you want to play again?");
+		}
+		i++;
 	}
-	i++;
-}
+// }
